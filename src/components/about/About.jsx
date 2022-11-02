2af3dd2a99fcd7efe5b530ce982888cc9225a392
Gabriel Chaves <gabovvv@gmail.com>
2022-11-02 14:57:53 -0600

    pdf compress

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import AboutImg from "../../assets/about.jpg";
-import CV from "../../assets/CVForWebGabrielCh.pdf";
+import CV from "../../assets/CVForWebGabrielCh_compressed.pdf";
 import { Info } from "./Info";
 
 export const About = () => {
